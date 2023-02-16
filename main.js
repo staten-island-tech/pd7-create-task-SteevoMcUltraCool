d20f739d373ba0c5b2db0a262ea823fa93b8d3c2
SteevoMcUltraCool <stephen.radice23@sitechhs.com>
2023-02-16 14:02:27 -0500

    wqeewqr

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-let ar = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","α","β","γ","δ"]
+let ar = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Α","Β","Γ","Δ","Ε"]
 const DOM = {
     inputSection: document.getElementById("inputs"),
     numberInput: document.getElementById("number"),
     initialBaseInput: document.getElementById("initialBase"),
     finalBaseInput: document.getElementById("finalBase"),
-    convertButton: document.getElementById("submit")
+    convertButton: document.getElementById("submit"),
+    resultsSection: document.getElementById("results")
 }
 function str(num){
   return ar[num]
 }
 function toBase10(n,b){
-    let n = String(n)
-    n = n.split("")
+    n = String(n)
+    n = n.toUpperCase().split("")
     n = n.reverse()
     let p = 0
     let s = 0
     n.forEach(d => {
         let rd = ar.findIndex(c => c == d)
-        rd = rd * (10**p)
+        if (rd > b-1){
+          return false
+        }
+        rd = rd * (b**p)
         s =s + rd
         p++
     });
@@ -25,10 +29,6 @@ function toBase10(n,b){
 }
 function  convert(x, b){
   let ret = "", rep = 1
-  if (b>ar.length){
-    console.error("Base too large. Maximum base currently supported is: "+ ar.length)
-    return false
-  }
   do{
     let d = x % b**rep
     x = x -d
@@ -39,5 +39,23 @@ function  convert(x, b){
   return ret
 }
 DOM.convertButton.addEventListener("click", function(){
-    
+  DOM.resultsSection.innerHTML = "<br>"
+  let num = DOM.numberInput.value
+  let iBase = Number(DOM.initialBaseInput.value)
+  let fBase = Number(DOM.finalBaseInput.value)
+  if (!num || num==""|| !iBase || iBase <2 || iBase>ar.length || !fBase || fBase<2 || fBase > ar.length){
+    DOM.resultsSection.insertAdjacentHTML("beforeend", `Make sure you have a number and both bases are between 2 and ${ar.length} (inclusive)`)
+    return "goop"
+  }
+  let b10Num = toBase10(num,iBase)
+  if (!b10Num){
+    DOM.resultsSection.insertAdjacentHTML("beforeend", `Makesure your number is valid in that base`)
+    return false
+  }
+  let cbNum = convert(b10Num, fBase)
+  DOM.resultsSection.insertAdjacentHTML("beforeend",`
+      <p>Your input : ${num}</p>
+      <p>Base 10 conversion: ${b10Num}</p>
+      <h3>Base ${fBase} result: ${cbNum} </h3>
+  `)
 })
